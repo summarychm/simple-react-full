d4c3c49895f7cdf54f0bb2889e631efa547745a0
max.liu <summarychm@hotmail.com>
2020-01-04 15:17:41 +0800

    🎨 完善redux-promise和redux-thunk的注释

--- src/Core/redux-promise/index.js
+++ src/Core/redux-promise/index.js
@@ -1,19 +1,14 @@
-function isPromise(obj) {
-  return (
-    !!obj &&
-    (typeof obj === 'object' || typeof obj === 'function') &&
-    typeof obj.then === 'function'
-  );
-}
+const isPromise = (obj) =>
+  !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function';
+
 export default ({ getState, dispatch }) => (next) => (action) => {
   console.log('redux-promise');
-  if (isPromise(action.payload)) {
-    return action.payload
-      .then((result) => dispatch({ ...action, payload: result }))
-      .catch((err) => {
-        dispatch({ ...action, payload: err, error: true });
-        return Promise.reject(err);
-      });
-  }
-  return next(action);
+  if (!isPromise(action.payload)) return next(action);
+
+  return action.payload
+    .then((result) => dispatch({ ...action, payload: result }))
+    .catch((err) => {
+      dispatch({ ...action, payload: err, error: true });
+      return Promise.reject(err);
+    });
 };
--- src/Core/redux-thunk/index.js
+++ src/Core/redux-thunk/index.js
@@ -1,8 +1,5 @@
 export default ({ dispatch, getState }) => (next) => (action) => {
   console.log('redux-thunk');
-  if (typeof action === 'function') {
-    console.log('111');
-    return action(dispatch, getState);
-  }
+  if (typeof action === 'function') return action(dispatch, getState);
   return next(action);
 };
